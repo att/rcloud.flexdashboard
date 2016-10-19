86ad0d26319613d23073d49309eab5a80d6d2b37
Gábor Csárdi <csardi.gabor@gmail.com>
2016-10-19 13:34:17 +0100

    Use rcloud.rmd to export Rmd, closes #4
    Also closes #2, rcloud.rmd handles chunk options.

--- R/render.R
+++ R/render.R
@@ -1,31 +1,9 @@
 
 renderFlexDashboard <- function(id, version = NULL) {
 
-  res <- rcloud.get.notebook(id, version)
-
-  if (! res$ok) return(NULL)
-
-  cells <- res$content$files
-  cells <- cells[grep("^part", names(cells))]
-  if (!length(names(cells))) return(NULL)
-
-  cnums <- suppressWarnings(as.integer(
-    gsub("^\\D+(\\d+)\\..*", "\\1", names(cells))
-  ))
-  cells <- cells[match(sort.int(cnums), cnums)]
-
   tmp <- tempfile(fileext = ".Rmd")
   on.exit(unlink(tmp), add = TRUE)
-  cat("", file = tmp)
-
-  for (cell in cells) {
-    if (grepl("^part.*\\.R$", cell$filename)) {
-      cat("\n```{r}\n", cell$content, "\n```\n", sep = "", file = tmp, append = TRUE)
-
-    } else if (grepl("^part.*\\.md$", cell$filename)) {
-      cat("\n", cell$content, "\n", sep = "", file = tmp, append = TRUE)
-    }
-  }
+  exportRmd(id, version, file = tmp)
 
   tmp2 <- tempfile(fileext = ".html")
   on.exit(unlink(tmp2), add = TRUE)
