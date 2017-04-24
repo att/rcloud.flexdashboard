90e6dc184463820e9d9a6347ac1fcfa9732e0174
Gordon Woodhull <gordon@woodhull.com>
2017-04-24 15:30:26 -0400

    lint (semicolons)
    purely subjective

--- inst/javascript/rcloud.flexdashboard.js
+++ inst/javascript/rcloud.flexdashboard.js
@@ -17,8 +17,8 @@
             } else {
                 onError = function(x) {
                     $('#rcloud-flexdashboard-loading').remove();
-                    RCloud.UI.fatal_dialog(x.message, "Close")
-                }
+                    RCloud.UI.fatal_dialog(x.message, "Close");
+                };
 
                 oc = RCloud.promisify_paths(ocaps, [
                     ['renderFlexDashboard']
@@ -27,25 +27,25 @@
                 window.RCloudFlexDashboard = window.RCloudFlexDashboard || {};
                 window.RCloudFlexDashboard.renderFlexDashboard = function(x, y) {
                     oc.renderFlexDashboard(x, y).catch(onError).then(function() {});
-                }
+                };
             }
 
-            k()
+            k();
         },
 
         render: function(target, html, k) {
             $('#rcloud-flexdashboard-loading').remove();
             var content = "<iframe frameBorder=\"0\" width=\"100%\" height=\"100%\" srcdoc=\"" + html + "\"></iframe>";
-            var parsed = $(html)
+            var parsed = $(html);
             var title = parsed.filter('title').text();
             if(title!=="") {
-              document.title = title
+                document.title = title;
             } else {
-              document.title = "RCloud flexdashboard"
-            }           
+                document.title = "RCloud flexdashboard";
+            }
             $(target).html(content);
             k(null, target);
         }
-    }
+    };
 
 })());
